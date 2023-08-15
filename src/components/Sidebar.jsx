3b07dabfd1f934fdf98e828eb9bd294600f5fd60
Esan Samuel <esansamuel555@gmail.com>
2023-08-15 18:54:39 +0100

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
     //#3a3a43
     return (
         <div className='bg-[#1c1c24] p-5 rounded card flex flex-col'>
-            <div className='bg-transparent sticky h-screen top-5 p-3 hover:overflow-auto'>
+            <div className='bg-transparent sticky h-screen top-5 p-3 overflow-hidden hover:overflow-auto'>
                 <h1 className='text-[30px] px-3 logo flex gap-3'><TbSocial className='bg-transparent text-[#00dbde]' />BuzzTalk</h1>
                 <ul className='flex flex-col mt-10 text-[#e1d9d1] space-y-3 cursor-pointer'>
                     <li onClick={() => setIsActive(true)} className={`flex gap-3 hover:bg-[#8c6dfd] hover:text-[#eaeaea] p-3 rounded ${isActive && 'bg-[#8c6dfd] text-[#eaeaea]'}`}><AiOutlineHome className='text-[20px] bg-transparent' /><span className=''>Home</span></li>
