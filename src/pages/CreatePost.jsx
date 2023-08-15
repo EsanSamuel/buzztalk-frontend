5617115966835fa0ba3ef79c2dbd9cfb6b81afd3
Esan Samuel <esansamuel555@gmail.com>
2023-08-15 22:35:01 +0100

    Update CreatePost.jsx

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -52,7 +52,7 @@ const CreatePost = () => {
             setLoading(true)
 
             try {
-                const response = await fetch('http://localhost:3001/api/v1/post', {
+                const response = await fetch('https://buzztalk-api-e1uj.vercel.app/api/v1/post', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
